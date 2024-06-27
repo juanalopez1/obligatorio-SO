6b538035afcbbcd0d3b8de0c97608690ac4e2231
Juana López <juana.lopez@correo.ucu.edu.uy>
2024-06-27 15:20:20 -0300

    punto 1 faltan permisos de no instalación y arreglar el if

--- parte-b/codigo/backup.ps1
+++ parte-b/codigo/backup.ps1
@@ -3,6 +3,6 @@ New-Item -ItemType Directory -Force -Path "$HOME\Comunicados\Semanal"
 New-Item -ItemType Directory -Force -Path "$HOME\Comunicados\Mensual"
 New-Item -ItemType Directory -Force -Path "$HOME\Respaldo"
 $rutaRespaldo = "$HOME\Respaldo"
-$date = Get-Date -Format "ddMMMMyyyy"
+$date = Get-Date -Format "ddMMMyyyy"
 $date = $date.toUpper()
 Compress-Archive -Path "$HOME\Comunicados" -DestinationPath "$rutaRespaldo\$date.zip"
